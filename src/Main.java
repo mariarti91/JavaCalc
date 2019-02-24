7410d0005cef850b2abe6ace760d4d82d175b673
Dmitriy Nikiforov <d.nikiforov@ion.team>
2019-02-24 15:41:02 +0700

    added simple GUI form

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Input your statement:");
-        String statement = in.nextLine();
-        System.out.println(MathParser.Calculate(statement));
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Input your statement:");
+//        String statement = in.nextLine();
+//        System.out.println(MathParser.Calculate(statement));
+        MainDialog md = new MainDialog();
+        md.pack();
+        md.setVisible(true);
+        System.exit(0);
     }
 }
---
+++ src/MainDialog.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainDialog extends JDialog {
+    private JPanel contentPane;
+    private JTextField tfStatement;
+    private JButton bCalc;
+
+    public MainDialog() {
+        setContentPane(contentPane);
+        setModal(true);
+
+        bCalc.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) { calculate(); }
+        });
+    }
+
+    private void calculate()
+    {
+        String statement = tfStatement.getText();
+        double result = MathParser.Calculate(statement);
+        tfStatement.setText(String.valueOf(result));
+    }
+}
